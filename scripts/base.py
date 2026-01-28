8844b42a78a42dd9d3415d8868fb2cab44336b8c
Dushnyj <asleksey.astashov@gmail.com>
2026-01-28 14:56:11 +0300

    Set Windows code page 866 for build output

--- scripts/base.py
+++ scripts/base.py
@@ -1390,6 +1390,8 @@ def vcvarsall_end():
 
 def run_as_bat(lines, is_no_errors=False):
   name = "tmp.bat" if ("windows" == host_platform()) else "./tmp.sh"
+  if ("windows" == host_platform()):
+    lines = ["chcp 866 >nul"] + lines
   content = "\n".join(lines)
 
   file = codecs.open(name, "w", "utf-8")
